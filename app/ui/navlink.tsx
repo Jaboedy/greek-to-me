5d4d01a205ebb99a74f0661b2dfdd37d5108fbad
Brody Underwood <tbunde01@gmail.com>
2025-07-02 23:02:53 -0400

    Fix styling of sidenav

--- app/ui/navlink.tsx
+++ app/ui/navlink.tsx
@@ -15,10 +15,12 @@ export const NavLink = ({href, label, icon}: NavLinkProps) => {
         href={href}
         className="flex items-center gap-2 text-blue-primary hover:text-blue-secondary transition-colors"
       >
-        <>
-            {icon ? icon : <></>}
-            <span className='text-blue-primary hover:text-gold-accent transition-colors text-lg font-semibold'>{label}</span>
-        </>
+        {icon && (
+          <div className="flex items-center justify-center w-5 h-5">
+            {icon}
+          </div>
+        )}
+        <span className='text-blue-primary hover:text-gold-accent transition-colors text-lg font-semibold'>{label}</span>
       </Link>
     </li>
   );
--- app/ui/sidenav.tsx
+++ app/ui/sidenav.tsx
@@ -49,7 +49,7 @@ export default function SideNav() {
 
             {/* Side Navigation */}
             <nav
-                className={`fixed top-0 left-0 h-full w-80 bg-greek-white shadow-xl transform transition-transform duration-300 ease-in-out z-50 ${isOpen ? 'translate-x-0' : '-translate-x-full'
+                className={`fixed top-0 left-0 h-full w-65 bg-greek-white shadow-xl transform transition-transform duration-300 ease-in-out z-50 ${isOpen ? 'translate-x-0' : '-translate-x-full'
                     }`}
             >
                 {/* Header */}
